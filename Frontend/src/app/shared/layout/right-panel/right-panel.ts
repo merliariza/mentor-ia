e01f3cc5ae4d8ba910be3c9969205bc8a2bee6ad
merliariza <180667185+merliariza@users.noreply.github.com>
2026-08-01 21:05:05 -0500

    feat: :sparkles: create application layout and feature structure

---
+++ Frontend/src/app/shared/layout/right-panel/right-panel.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-right-panel',
+  imports: [],
+  templateUrl: './right-panel.html',
+  styleUrl: './right-panel.css',
+})
+export class RightPanel {
+
+}
